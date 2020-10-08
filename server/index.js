8214f3f38dc0f52f7eeb372b06ab3f7ec47f0920
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-08 15:26:04 -0500

    Proxy now accommodates product_id selection in URL

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const app = express();
 const port = 3099;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/')));
+
+app.get('/:product_id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Server is listening at: ${port}`);
