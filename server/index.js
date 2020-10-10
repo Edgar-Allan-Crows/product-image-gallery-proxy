c0d0709a37ab97968135124445270511264f1740
Ubuntu <ubuntu@ip-172-31-41-197.us-east-2.compute.internal>
2020-10-10 23:10:01 +0000

    Proxy works with p.id

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const port = 3099;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/:product_id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is listening at: ${port}`);
 });
