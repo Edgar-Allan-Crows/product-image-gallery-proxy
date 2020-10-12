6c97d232fa1a03249845e01a568ccb6df8b7e051
Ubuntu <ubuntu@ip-172-31-41-197.us-east-2.compute.internal>
2020-10-12 20:45:38 +0000

    Uses HPM for service routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
 const port = 3099;
+const hostname = 'http://34.228.73.56:3000'
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/api/totalScore/:product_id', createProxyMiddleware({ target: hostname, changeOrigin: true }));
+app.use('/api/reviewCount/:product_id', createProxyMiddleware({ target: hostname, changeOrigin: true }));
+// app.use('/:product_id', createProxyMiddleware({ target: hostname, changeOrigin: true}));
 
 app.get('/:product_id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
